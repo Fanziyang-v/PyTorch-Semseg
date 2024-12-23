18103f2a61cb4ef5ac1c41960fd078e7a0ed5aa6
ziyangfan <1229417754@qq.com>
2024-12-24 01:22:28 +0800

    fix bugs.

--- models/heads/psp_head.py
+++ models/heads/psp_head.py
@@ -7,26 +7,29 @@ class PSPHead(nn.Module):
     """PSPNet head module for segmentation."""
 
     def __init__(
-        self, in_channels: int, num_classes: int, pool_sizes: tuple = (1, 2, 3, 6)
+        self, in_channels: int, channels: int, pool_sizes: tuple = (1, 2, 3, 6)
     ):
         super(PSPHead, self).__init__()
-        self.ppm = PyramidPoolingModule(in_channels, pool_sizes)
-        self.fc = nn.Conv2d(in_channels * 2, num_classes, kernel_size=1)
+        self.ppm = PyramidPoolingModule(in_channels, channels, pool_sizes)
+        self.bottleneck = nn.Conv2d(
+            in_channels + len(pool_sizes) * channels, channels, kernel_size=1
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.ppm(x)
-        x = self.fc(x)
+        x = self.bottleneck(x)
         return x
 
 
 class PyramidPoolingModule(nn.Module):
     """Pyramid Pooling Module used in PSPNet."""
 
-    def __init__(self, in_channels: int, pool_sizes: tuple = (1, 2, 3, 6)):
+    def __init__(
+        self, in_channels: int, channels: int, pool_sizes: tuple = (1, 2, 3, 6)
+    ):
         super(PyramidPoolingModule, self).__init__()
-        out_channels = in_channels // len(pool_sizes)
         self.stages = nn.ModuleList(
-            [self._make_stage(in_channels, out_channels, size) for size in pool_sizes]
+            [self._make_stage(in_channels, channels, size) for size in pool_sizes]
         )
         self.relu = nn.ReLU(inplace=True)
 
@@ -41,8 +44,8 @@ class PyramidPoolingModule(nn.Module):
             )
         return torch.cat(pyramids, dim=1)
 
-    def _make_stage(self, in_channels: int, out_channels: int, bin_size: int):
+    def _make_stage(self, in_channels: int, channels: int, bin_size: int):
         """AdaptiveAvgPool2d + 1x1 Conv2d."""
         prior = nn.AdaptiveAvgPool2d((bin_size, bin_size))
-        conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        conv = nn.Conv2d(in_channels, channels, kernel_size=1, bias=False)
         return nn.Sequential(prior, conv)
